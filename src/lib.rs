1ae1b14309d25cf4e310cbb2b9eaff068c0f77c3
方法放寒假 <40489495+MiemieMethod@users.noreply.github.com>
2025-01-10 17:16:56 +0800

    implement infinity nikki

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,7 @@
 #![feature(ptr_sub_ptr)]
-#![feature(lazy_cell)]
 #![allow(improper_ctypes_definitions)]
 
-pub mod custom;
-pub mod lua51;
+pub mod lua54;
 
 use luac_parser::*;
 #[allow(unused_imports)]
@@ -20,46 +18,49 @@ use nom::{
     sequence::{delimited, tuple},
 };
 use nom::{
-    combinator::success,
-    error::{context, ErrorKind, ParseError},
-    multi::{length_count, length_data},
+    bytes::complete::take,
+    error::{context},
+    multi::{length_count},
     number::complete,
     Parser,
 };
 use nom_supreme::{error::*, ParserExt};
+use std::collections::HashMap;
 
 fn lua_header(input: &[u8]) -> IResult<&[u8], LuaHeader, ErrorTree<&[u8]>> {
     let (rest, (_, result)) = tuple((
         tag(b"\x1BLua"),
         alt((map(
             tuple((
-                tag(b"\x51"),
+                tag(b"\x53"),
+                be_u8,
+                take(6usize), // LUAC_DATA
                 be_u8,
                 be_u8,
                 be_u8,
-                be_u8,
-                be_u8,
-                be_u8,
+                complete::le_i64,
+                complete::le_f64,
                 be_u8,
             )),
             |(
-                _,
+                 _,
+                 format_version,
+                 _luac_data,
+                 instruction_size,
+                 _integer_size, // lua_Integer
+                 number_size,
+                 _,
+                 _,
+                 _,
+             )| LuaHeader {
+                lua_version: LUA54.0,
                 format_version,
-                big_endian,
-                int_size,
-                size_t_size,
+                big_endian: false,
+                int_size: 4,
+                size_t_size: 8,
                 instruction_size,
                 number_size,
-                number_integral,
-            )| LuaHeader {
-                lua_version: 0x51,
-                format_version,
-                big_endian: big_endian != 1,
-                int_size,
-                size_t_size,
-                instruction_size,
-                number_size,
-                number_integral: number_integral != 0,
+                number_integral: false,
                 ..Default::default()
             },
         ),)),
@@ -67,97 +68,15 @@ fn lua_header(input: &[u8]) -> IResult<&[u8], LuaHeader, ErrorTree<&[u8]>> {
     Ok((rest, result))
 }
 
-fn must<I, O, E: ParseError<I>, P: Parser<I, O, E>>(
-    cond: bool,
-    mut parser: P,
-) -> impl FnMut(I) -> IResult<I, O, E> {
-    move |input| {
-        if cond {
-            parser.parse(input)
-        } else {
-            Err(nom::Err::Error(E::from_error_kind(
-                input,
-                ErrorKind::Switch,
-            )))
-        }
-    }
-}
-
-fn lua_int<'a>(header: &LuaHeader) -> impl Parser<&'a [u8], u64, ErrorTree<&'a [u8]>> {
-    let intsize = header.int_size;
-    alt((
-        must(
-            intsize == 8,
-            map(complete::u64(header.endian()), |v| v as u64),
-        ),
-        must(
-            intsize == 4,
-            map(complete::u32(header.endian()), |v| v as u64),
-        ),
-        must(
-            intsize == 2,
-            map(complete::u16(header.endian()), |v| v as u64),
-        ),
-        must(intsize == 1, map(be_u8, |v| v as u64)),
-    ))
-    .context("integer")
-}
-
-fn lua_size_t<'a>(header: &LuaHeader) -> impl Parser<&'a [u8], u64, ErrorTree<&'a [u8]>> {
-    let sizesize = header.size_t_size;
-    alt((
-        must(
-            sizesize == 8,
-            map(complete::u64(header.endian()), |v| v as u64),
-        ),
-        must(
-            sizesize == 4,
-            map(complete::u32(header.endian()), |v| v as u64),
-        ),
-        must(
-            sizesize == 2,
-            map(complete::u16(header.endian()), |v| v as u64),
-        ),
-        must(sizesize == 1, map(be_u8, |v| v as u64)),
-    ))
-    .context("size_t")
-}
-
-fn lua_number<'a>(header: &LuaHeader) -> impl Parser<&'a [u8], LuaNumber, ErrorTree<&'a [u8]>> {
-    let int = header.number_integral;
-    let size = header.number_size;
-    alt((
-        must(
-            int == true,
-            map(
-                alt((
-                    must(size == 1, map(complete::be_i8, |v| v as i64)),
-                    must(size == 2, map(complete::i16(header.endian()), |v| v as i64)),
-                    must(size == 4, map(complete::i32(header.endian()), |v| v as i64)),
-                    must(size == 8, map(complete::i64(header.endian()), |v| v as i64)),
-                )),
-                |v| LuaNumber::Integer(v),
-            ),
-        ),
-        must(
-            int == false,
-            map(
-                alt((
-                    must(size == 8, map(complete::f64(header.endian()), |v| v as f64)),
-                    must(size == 4, map(complete::f32(header.endian()), |v| v as f64)),
-                )),
-                |v| LuaNumber::Float(v),
-            ),
-        ),
-    ))
-    .context("number")
-}
-
 pub fn lua_bytecode(input: &[u8]) -> IResult<&[u8], LuaBytecode, ErrorTree<&[u8]>> {
-    let (input, header) = lua_header(input)?;
+    let (input, header) = alt((lua_header, luajit::lj_header))(input)?;
     log::trace!("header: {header:?}");
-    let (input, main_chunk) = match header.lua_version {
-        0x51 => lua51::lua_chunk(&header).parse(input)?,
+    let (input, main_chunk) = match header.version() {
+        LUA51 => lua51::lua_chunk(&header).parse(input)?,
+        LUA52 => lua52::lua_chunk(&header).parse(input)?,
+        LUA53 => lua53::lua_chunk(&header).parse(input)?,
+        LUA54 => lua54::lua_chunk(&header).parse(input)?,
+        LUAJ1 | LUAJ2 => luajit::lj_chunk(&header).parse(input)?,
         _ => context("unsupported lua version", fail)(input)?,
     };
     Ok((input, LuaBytecode { header, main_chunk }))
